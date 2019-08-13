caa93cc83a9433513006d86d594fdbf79199bc65
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-08-12 23:44:29 -0500

    updated portfolio

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,26 +10,21 @@ export default class Header extends Component {
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               <strong><li className="current"><a className="smoothscroll" href="#home">Home</a></li></strong>
+               <strong><li><a className="smoothscroll" href="#about">About</a></li></strong>
+               <strong><li><a className="smoothscroll" href="#resume">Resume</a></li></strong>
+               <strong><li><a className="smoothscroll" href="#portfolio">Portfolio</a></li></strong>
+               <strong><li><a className="smoothscroll" href="#contact">Contact</a></li></strong>
             </ul>
          </nav>
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">Kelsey Gibbs</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>Junior Software Developer with a passion for innovation and problem solving. 
-               </h3>
+            <strong><h1 className="responsive-headline">Kelsey Gibbs</h1></strong>
+            <strong><h3 style={{color:'#fff', fontFamily:'sans-serif '}}>Junior Software Developer with a passion for innovation and problem solving. 
+               </h3></strong>
                <hr/>
-               <ul className="social">
-\\
-                              <li key="bookstore">
-                              </li>
-                  }
-               </ul>
+
             </div>
          </div>
 
--- client/src/resumeData.js
+++ client/src/resumeData.js
@@ -2,9 +2,7 @@ let resumeData = {
     "imagebaseurl":"https://kelseygibbs.github.io/",
     "name": "Kelsey Gibbs",
     "role": "Junior Software Developer with a passion for innovation and problem solving.",
-    "linkedinId":"",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "linkedinId":"3187813",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -31,17 +29,24 @@ let resumeData = {
     "work":[
       {
         "CompanyName":"Trilogy Education at Vanderbilt University",
-        "specialization":"Full-Stack Web Developer Teaching Assistant",
+        "specialization":"Teaching Assistant",
         "MonthOfLeaving":"July 2019 - ",
-        "YearOfLeaving":"Current",
-        "Achievements":"Teaching students full-stack web development using HTML, CSS, JavaScript, jQuery, Bootstrap, Express, React, Node, MySQL, MongoDB, and more through an intensive 24-week curriculum. Code reviewing students web application assignments and leading presentations on JavaScript concepts."
+        "YearOfLeaving":"Present",
+        "Achievements":"Assisting the instuctor and helping teach students full-stack web development using HTML, CSS, JavaScript, jQuery, Bootstrap, Express, React, Node, MySQL, MongoDB, and more through an intensive 24-week curriculum. Code reviewing students web application assignments and leading presentations on JavaScript concepts."
       },
       {
         "CompanyName":"Ryman Hospitality Properties",
         "specialization":"Pastry Chef",
+        "MonthOfLeaving":"Jan 2017 - ",
+        "YearOfLeaving":"Mar 2018",
+        "Achievements":"Crafted and tested menus for two new restaurant concepts and a busy downtown banquet space to ensure the best product for the brand. Recruited, interviewed, trained, and scheduled pastry cooks to accommodate the volume of these new Broadway hotspots."
+      },
+      {
+        "CompanyName":"City Winery - Nashville",
+        "specialization":"Pastry Chef",
         "MonthOfLeaving":"Mar 2018 - ",
         "YearOfLeaving":"Jan 2018",
-        "Achievements":"Crafted and tested menus for two new restaurant concepts and a busy downtown banquet space to ensure the best product for the brand. Recruited, interviewed, trained, and scheduled pastry cooks to accommodate the volume of these new Broadway hotspots."
+        "Achievements":"I was hired as an assistant to the pastry chef to maintain and expand the bread program. I created and costed recipes and began to assist with pastry production as needed. A short time later, I was promoted to Pastry Chef and charged with menu development, food purchasing, as well as employee training and development."
       }
     ],
     "skills":[
@@ -62,24 +67,22 @@ let resumeData = {
       {
         "name":"Mongo News Scraper",
         "description":"A web app that lets users view and leave comments on the latest news. When a user visits the site, the app scrapes news stories and displays them for users to comment on. Used: NodeJS, Express, Express-Handlebars, Mongoose, Cheerio, Axios.",
-        "imgurl":""
+        "imgurl":"https://live.staticflickr.com/65535/48521255746_ca462187b5.jpg",
+        "link":""
+      },
+      {
+        "name":"Voila",
+        "description":"An interactive website to find live music events, happening tonight in the city of your choice. Used: Javascript, JQuery, AJAX, TicketMaster API, last.fm API",
+        "imgurl":"https://live.staticflickr.com/65535/48521595272_f03bd01761.jpgg",
+        "link":""
       }
-      // ,
-      // {
-      //   "name":"project2",
-      //   "description":"mobileapp",
-      //   "imgurl":"images/portfolio/project.jpg"
-      // },
-      // {
-      //   "name":"project3",
-      //   "description":"mobileapp",  
-      //   "imgurl":"images/portfolio/project2.png"
-      // },
-      // {
-      //   "name":"project4",
-      //   "description":"mobileapp",
-      //   "imgurl":"images/portfolio/phone.jpg"
-      // }
+      ,
+      {
+        "name":"WholeBaker",
+        "description":"A web application for wholesale bakery sales management. Role-based authentication leading clients to a store, and leading admin to a product management portal. Used: React.js, Redux, MongoDB, Mongoose, Node.Js, PassportJWT, Materialize-CSS",  
+        "imgurl":"images/portfolio/project2.png",
+        "link":""
+      }
     ]
   }
   
