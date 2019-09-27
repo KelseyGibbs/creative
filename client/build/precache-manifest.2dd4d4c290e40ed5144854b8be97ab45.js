b35f3d03c062e1db5c9bc4c0ac80a612d85eaa76
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-09-27 09:43:17 -0500

    build client

---
+++ client/build/precache-manifest.2dd4d4c290e40ed5144854b8be97ab45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ffa8ce871392496405",
+    "url": "/static/css/main.543abdcc.chunk.css"
+  },
+  {
+    "revision": "b8ffa8ce871392496405",
+    "url": "/static/js/main.8509edbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "97a7dc122561f58515dd8dabb955a0cc",
+    "url": "/index.html"
+  }
+];
